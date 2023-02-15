0aa3aa378cd74b0d2dc6b89b3064b1d9ec9a3d6f
joohwan <joohwan9607@mobility42.io>
2023-02-15 19:54:11 +0900

    Log 관련 라이브러리

---
+++ src/main/java/com/sjh/springmvc1_1/springmvc/basic/LogTestController.java
@@ -0,0 +1,25 @@
+package com.sjh.springmvc1_1.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LogTestController {
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+
+        return "ok";
+    }
+}
