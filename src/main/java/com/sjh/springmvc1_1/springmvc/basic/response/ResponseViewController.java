c6700f94462098b0b62d0787e66818f012193e9b
joohwan9607 <joohwan9607@mobility42.io>
2023-02-19 18:49:16 +0900

    response - static resource, view templates

---
+++ src/main/java/com/sjh/springmvc1_1/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,24 @@
+package com.sjh.springmvc1_1.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView mv = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mv;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data", "data");
+        return "response/hello";
+    }
+}
